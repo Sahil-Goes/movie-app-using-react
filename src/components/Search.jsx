29baeab8a14095dd68340bb21a28df9262b3a612
Sahil Goes <sahilgoes87@gmail.com>
2025-06-11 01:41:27 +0530

    Movie app using React, TMDB API, Tailwind, and Appwrite

---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+//Search component to search for movie using api
+const Search = ({ searchTerm, setSearchTerm}) => {
+  
+    return (
+    <div className='search'>
+        <div>
+            <img src="search.svg" alt="search" />
+
+            <input 
+                type="text" 
+                placeholder="Search through thousands of movies"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value) }
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Search
